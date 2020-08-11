c48c021e6b68ac5388d1d867bb75c4cf84b9ecab
Evgeny Malikov <mev@offershub.ru>
2020-08-11 11:13:34 +0500

    change main

--- main.py
+++ main.py
@@ -1,43 +1,110 @@
 from psycopg2 import connect
 
-with connect('dbname=netology user=postgres host=localhost port=5432') as conn:
-    with conn.cursor() as cur:
-        cur.execute(
-            """
-            create table if not exists Genre(
-                name varchar(20) primary key 
+def create_music_db():
+    with connect('dbname=netology user=postgres host=localhost port=5432') as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Genre(
+                    name VARCHAR(20) PRIMARY KEY
+                )
+                """
             )
-            """
-        )
 
-        cur.execute(
-            """
-            create table if not exists Artist (
-                id serial primary key,
-                name varchar(50) not null,
-                genre_name varchar(20) references Genre(name)
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Artist (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL
+                )
+                """
             )
-            """
-        )
 
-        cur.execute(
-            """
-            create table if not exists Album(
-                id serial primary key,
-                name varchar(100) not null,
-                release_date date not null ,
-                artist_id integer references Artist(id)               
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS ArtistGenre(
+                    artist_id INTEGER,
+                    genre_id VARCHAR(20),
+                    CONSTRAINT pk_artist_genre PRIMARY KEY (artist_id, genre_id)
+                )
+                """
             )
-            """
-        )
 
-        cur.execute(
-            """
-            create table if not exists Song(
-                id serial primary key,
-                name varchar(100) not null,
-                duraction integer not null check ( duraction > 0 ),
-                album_id integer references Album(id)
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Album(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL,
+                    release_date DATE
+                )
+                """
             )
-            """
-        )
+
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS ArtistAlbum(
+                    artist_id INTEGER,
+                    album_id INTEGER,
+                    CONSTRAINT pk_artist_album PRIMARY KEY (artist_id, album_id)
+                )
+                """
+            )
+
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Song(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL,
+                    duraction INTEGER NOT NULL CHECK ( duraction > 0 ),
+                    album_id INTEGER REFERENCES Album(id)
+                )
+                """
+            )
+
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Collection(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL,
+                    release_date DATE
+                )
+                """
+            )
+
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS SongCollection(
+                    song_id INTEGER,
+                    collection_id INTEGER,
+                    CONSTRAINT pk_song_collection PRIMARY KEY (song_id, collection_id)
+                )
+                """
+            )
+
+def creat_employee_db():
+    with connect('dbname=netologydb user=postgres host=localhost port=5432') as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Department(
+                    name VARCHAR(20) PRIMARY KEY 
+                )
+                """
+            )
+
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Employee(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(50),
+                    boss_id INTEGER REFERENCES Employee(id),
+                    department_name VARCHAR(20) REFERENCES Department(name)
+                    
+                )
+                """
+            )
+
+
+if __name__ == '__main__':
+    # create_music_db()
+    creat_employee_db()
